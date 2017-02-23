69952393249114a09fc45318136a834db3df9864
Saliot <saliotj@gmail.com>
2017-02-23 19:51:11 +0100

    eReporting

--- eReporting/src/main/java/com/eReporting/repository/ScrollingChapterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eReporting.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eReporting.model.ScrollingChapter;
-
-public interface ScrollingChapterRepository extends JpaRepository<ScrollingChapter, Long>{
-
-}
